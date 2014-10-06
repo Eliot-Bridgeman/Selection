38550e67a047d9bdab3eca17c98bcb636bce1d32
Eliot Bridgeman <44991@longroad.ac.uk>
2014-10-06 09:47:15 +0100

    First if program

---
+++ selection revision 1.py
@@ -0,0 +1,11 @@
+#Eliot Bridgeman
+#6/10/14
+#selection excercise 1
+
+number1 = int(input("please enter a number"))
+number2 = int(input("please enter another number"))
+if number1 == number2:
+              print("The two numbers are the same")
+else:
+    print("they are not the same")
+        
