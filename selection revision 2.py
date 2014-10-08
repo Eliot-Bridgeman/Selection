fa9ad66fbd95fe74b4297eb54ec600fd2070b232
Eliot Bridgeman <44991@longroad.ac.uk>
2014-10-08 14:04:17 +0100

    Selection revision

---
+++ selection revision 2.py
@@ -0,0 +1,9 @@
+#Eliot Bridgeman
+#6/10/14
+#selection revision
+age = int(input("please enter your age"))
+if age >= 17:
+    print("you are old enough to drive")
+elif age < 17:
+    print("you are not old enough to drive")
+
---
+++ selection revision 3.py
@@ -0,0 +1,11 @@
+#Eliot Bridgeman
+#6/10/14
+#selection revision 3
+number = int(input("please enter a number"))
+if number < 21:
+    print("the number is too small")
+elif number > 29:
+    print("The number is too high")
+else:
+    print("The number is in the range")
+    
